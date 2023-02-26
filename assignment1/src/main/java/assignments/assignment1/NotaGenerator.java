2bda639a0f000d68ddd936aed8fa6e7e0a8c3c0c
Fadeeaal <rakha.fadil@ui.ac.id>
2023-02-26 22:58:17 +0700

    add: menambahkan switch case untuk tiga pilihan dan membuat isi method generateId

--- assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
+++ assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
@@ -10,10 +10,12 @@ public class NotaGenerator {
      */
     public static void main(String[] args) {
         boolean runningProgram = false;
-        int kodePelanggan = 0;
+        String nomorHandphone = ""; String namaPelanggan = "";
         
         while (!runningProgram){
+            boolean isNumber = false;
             printMenu();
+
             System.out.print("Pilihan : ");
             String pilihan = input.nextLine();
             System.out.println("================================");
@@ -21,15 +23,70 @@ public class NotaGenerator {
                 case "1":
                 //Generate ID
                 System.out.println("Masukkan nama Anda: ");
-                String namaPelanggan = input.nextLine();
+                namaPelanggan = input.nextLine();
                 
                 System.out.println("Masukkan nomor handphone Anda: ");
-                int nomorHandphone = input.nextInt();
-                
+                while (!isNumber){
+                    if (input.hasNextLong()){
+                        nomorHandphone = Long.toString(input.nextLong());
+                        isNumber = true;
+                    }
+                    else{
+                        System.out.println("Nomor hp hanya menerima digit");
+                        input.nextLine();
+                    }
+                }
+                System.out.println(generateId(namaPelanggan, nomorHandphone));
                 break;
 
                 case "2":
                 //Generate Nota
+                String paketLaundry;
+                System.out.println("Masukkan nama Anda: ");
+                namaPelanggan = input.next();
+                input.nextLine();
+
+                System.out.println("Masukkan nomor handphone Anda: ");
+                while (!isNumber) {              
+                    if(input.hasNextLong()) {
+                        nomorHandphone = Long.toString(input.nextLong());
+                        isNumber = true;
+                    } 
+                    else {
+                        System.out.println("Nomor hp hanya menerima digit");
+                        input.nextLine();
+                    }
+                  }
+
+                System.out.println("Masukkan tanggal terima: ");
+                String tanggalTerima = input.next();
+                input.nextLine();
+
+                do {
+                    System.out.println("Masukkan paket laundry:");
+                    paketLaundry = input.nextLine().toLowerCase();
+                    switch (paketLaundry) {
+                        case "express": case "fast": case "reguler":
+                        break;
+                        
+                        case "?":
+                        showPaket();
+                        break;
+
+                        default:
+                        System.out.println("Paket " + paketLaundry + " tidak diketahui\n[ketik ? untuk mencari tahu jenis paket]");
+                        break;
+                    }
+                } while (!paketLaundry.equals("express") && !paketLaundry.equals("fast") && !paketLaundry.equals("reguler"));
+                
+                System.out.println("Masukkan berat cucian anda [kg]: ");
+                int beratCucian = input.nextInt();
+                input.nextLine();
+                if (beratCucian < 2){
+                    System.out.println("Cucian kurang dari 2 kg, maka cucian akan dianggap sebagai 2 kg");
+                    beratCucian = 2;
+                }
+                System.out.println(generateNota(generateId(namaPelanggan, nomorHandphone), paketLaundry, beratCucian, tanggalTerima));
                 break;
 
                 case "0":
@@ -42,7 +99,6 @@ public class NotaGenerator {
                 break;
             }
         }
-        
     }
 
     /**
@@ -74,8 +130,48 @@ public class NotaGenerator {
      * @return String ID anggota dengan format [NAMADEPAN]-[nomorHP]-[2digitChecksum]
      */
     public static String generateId(String nama, String nomorHP){
-        // TODO: Implement generate ID sesuai soal.
-        return null;
+        String namaPelanggan = "", idPelanggan;
+        int checkSum = 0;
+
+        for (int i = 0; i < nama.length(); i++){
+            if (nama.charAt(i) == ' '){
+                break;
+            } else{
+                namaPelanggan += nama.charAt(i);
+            }
+        }
+
+        if (nomorHP.substring(0, 1).equals("0")){
+            nomorHP = "0" + nomorHP;
+        }
+        
+        idPelanggan = namaPelanggan.toUpperCase() + "-" + nomorHP;
+        for (int j = 0; j < idPelanggan.length(); j++){
+            if (Character.isLetter(idPelanggan.charAt(j))){
+                int hurufAngka = Character.getNumericValue(idPelanggan.charAt(j)) - 9;
+                checkSum += hurufAngka;
+            }
+            else if (Character.isDigit(idPelanggan.charAt(j))){
+                int angkaAngka = Character.getNumericValue(idPelanggan.charAt(j));
+                checkSum += angkaAngka;
+            }
+            else{
+                checkSum += 7;
+            }
+        }
+        String kodeAngka = Integer.toString(checkSum);
+        if (checkSum < 10){
+            kodeAngka = "0" + kodeAngka;
+            return (idPelanggan + "-" + kodeAngka);
+        }
+        else{
+            if (kodeAngka.length() > 2){
+                return (idPelanggan + "-" + kodeAngka.substring(kodeAngka.length()-2, kodeAngka.length()));
+            }
+            else{
+                return (idPelanggan + "-" + kodeAngka);
+            }
+        }
     }
 
     /**
